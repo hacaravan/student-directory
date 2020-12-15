0c10055e6397c15ae17eed3f825a3305fa2d10f6
hacaravan <hacavanagh@gmail.com>
2020-12-15 11:10:37 +0000

    Corrected misplaced commas

--- directory.rb
+++ directory.rb
@@ -1,14 +1,14 @@
-students = ["Dr Hannibal Lecter"
-,"Darth Vader"
-,"Nurse Ratched"
-,"Michael Corleone"
-,"Alex DeLarge"
-,"The Wicked Witch of the West"
-,"Terminator"
-,"Freddy Krueger"
-,"The Joker"
-,"Joffrey Baratheon"
-,"Norman Bates"]
+students = ["Dr Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"]
 
 puts "The students of Villains Academy"
 
