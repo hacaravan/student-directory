a474d49e1c1727afc3760c7b4ec4dcb6a7dfc862
hacaravan <hacavanagh@gmail.com>
2020-12-15 13:32:24 +0000

    Added separate methods to question 2 if you want to select a particular starting letter to filter by

--- Exercises/directory_exercises_2.rb
+++ Exercises/directory_exercises_2.rb
@@ -14,9 +14,9 @@ def input_students
   students
 end
 
-def get_letter
+def get_starting_letter
   while true do
-    puts "Please enter a single letter, or press enter to ignore"
+    puts "Please enter a single letter that each name should start with, or press enter to ignore"
     letter = gets.chomp
     return letter if letter.length == 1
     return nil if letter.empty?
@@ -34,13 +34,35 @@ def print_names(students)
   end
 end
 
+def print_names_starting_with(students, initial)
+  students.each_with_index do |student, index|
+    if student[:name].downcase.chr == initial.downcase
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+end
+
 def print_footer(students)
   puts "Overall, we have #{students.count} great students"
 end
-let = get_letter
-puts let == nil ? "No letter given" : let
 
-# students = input_students
-# print_header
-# print_names(students)
-# print_footer(students)
+def print_footer_starting_with(students, initial, initial_count)
+  puts "Overall, we have #{students.count} great students"
+  puts "Of those, #{initial_count} have a name starting with #{initial}"
+end
+
+def print_names_and_footer(students, initial)
+  if initial.nil?
+    print_names(students)
+    print_footer(students)
+  else
+    student_initial_count = students.select{ |student| student[:name].chr.downcase == initial.downcase }.length
+    print_names_starting_with(students, initial)
+    print_footer_starting_with(students, initial, student_initial_count)
+  end
+end
+
+students = input_students
+initial = get_starting_letter
+print_header
+print_names_and_footer(students, initial)
