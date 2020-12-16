82013e43deb91ad92bc1c12076a02bfed11fcc97
hacaravan <hacavanagh@gmail.com>
2020-12-16 13:43:04 +0000

    Added method to get valid cohor from user

--- Exercises/directory_exercises_7.rb
+++ Exercises/directory_exercises_7.rb
@@ -1,10 +1,10 @@
-# In this exercise, we want to prompt the user for a hobby, country of birth,
-# height etc for each student
+# In this exercise, we want to prompt the user for the cohort that each student is in
+# We also need to check that that cohort is a valid one (for now assume all months are valid)
 
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
-  info_categories = [:name, :cohort, :hobby, :birthplace, :height]
+  info_categories = [:name, :cohort]
   # Create empty array to be filled with students
   students = []
   while true do
@@ -19,6 +19,17 @@ def input_students
     students << student_hash
     puts "Now we have #{students.length} students"
   end
+end
+
+def get_valid_cohort
+  valid_cohorts = [:January, :February, :March, :April, :May, :June, :July,
+                  :August, :September, :November, :December]
+  cohort = String.new
+  while !valid_cohorts.include?(cohort)
+    puts "What cohort is this student in?"
+    cohort = gets.chomp.capitalize.to_sym
+  end
+  cohort
 end
 
 def print_header
@@ -41,7 +52,9 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great student#{students.length == 1 ? "" : "s"}"
 end
 
-students = input_students
-print_header
-print_names(students)
-print_footer(students)
+get_valid_cohort
+
+# students = input_students
+# print_header
+# print_names(students)
+# print_footer(students)
