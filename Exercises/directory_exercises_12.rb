c3d9da89f09473a8f85be7b7506ba2f0bdc49cc3
hacaravan <hacavanagh@gmail.com>
2020-12-17 17:38:35 +0000

    Finished directory exercise 12 - dealing with an empty list of students

---
+++ Exercises/directory_exercises_12.rb
@@ -0,0 +1,43 @@
+# This time, we will not print out the list of students if the user hasn't entered any
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  # Create empty array to be filled with students
+  students = []
+  # get first name
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------------"
+end
+
+def print_names(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+def print_no_names
+  puts "Since you did not enter any names, Villains Academy is empty and had to close"
+end
+
+students = input_students
+if students.empty?
+  print_no_names
+else
+  print_header
+  print_names(students)
+  print_footer(students)
+end
