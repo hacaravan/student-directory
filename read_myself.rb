25b91c71c2a3806d9faac615095d9f3521cca23c
hacaravan <hacavanagh@gmail.com>
2020-12-18 19:27:34 +0000

    Added functionality to read_myself.rb to be able to read other files based on user input

--- read_myself.rb
+++ read_myself.rb
@@ -1,3 +1,25 @@
-# This is a Ruby program that should be able to read itself
-file_name = __FILE__
-puts File.read(file_name)
+# This is a Ruby program that should be able to read itself, or other files you ask it to
+@file_name = __FILE__
+
+def read_file_contents
+  puts "\nThe contents of the file #{@file_name} are:\n\n#{File.read(@file_name)}"
+end
+
+def get_existing_file
+  loop do
+    puts "\nPlease enter a file name to read, or type 'exit' to stop this program"
+    @file_name = gets.chomp
+    return if File.exist?(@file_name)
+    exit if @file_name == "exit"
+    puts "\nSorry, that file doesn't exist"
+  end
+end
+
+def print_files
+  loop do
+    read_file_contents
+    get_existing_file
+  end
+end
+
+print_files
