28e2dd527fa71f1dbde599419c8cd28bb1b02054
hacaravan <hacavanagh@gmail.com>
2020-12-18 13:07:54 +0000

    Copied existing directory.rb file to new directory more exercises file in Exercises folder

---
+++ Exercises/directory_more_exercises.rb
@@ -0,0 +1,96 @@
+
+@students = []
+
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  # get first name
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save student list to students.csv"
+  puts "4. Load student list from students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1" then input_students
+  when "2" then show_students
+  when "3" then save_students
+  when "4" then load_students
+  when "9" then exit
+  else
+    puts "I didn't catch that, please try again"
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_students(file_name = "students.csv")
+  file = File.open(file_name, "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  file_name = ARGV.first
+  return if file_name.nil?
+  if File.exists?(file_name)
+    load_students(file_name)
+    puts "Loaded #{@students.length} from #{file_name}"
+  else
+    puts "Sorry, the file '#{file_name}' doesn't exist"
+    exit
+  end
+end
+
+try_load_students
+interactive_menu
